b35a30bff194dafd7341beb361e37a10cc2e9acb
Marcelo Canaparro <canaparro@Marcelos-MacBook-Pro.local>
2020-12-12 07:05:27 -0300

    adds missing files

---
+++ src/main/java/com/example/demo/HospitalWatchApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalWatchApplication.class, args);
+	}
+
+}
